debcde219b54044c21aabf0df36fb3fc72458161
macsoftware <87543117+macsoftware@users.noreply.github.com>
2023-01-19 22:59:48 +0000

    Set up OneSignal to send push notifications

--- FirebaseIGCloneExample/AppDelegate.swift
+++ FirebaseIGCloneExample/AppDelegate.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import FirebaseCore
+import OneSignal
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        // Remove this method to stop OneSignal Debugging
+          OneSignal.setLogLevel(.LL_VERBOSE, visualLevel: .LL_NONE)
+          
+          // OneSignal initialization
+          OneSignal.initWithLaunchOptions(launchOptions)
+          OneSignal.setAppId("79cd9f98-7a69-4b64-b857-34353c536859")
+          
+          // promptForPushNotifications will show the native iOS notification permission prompt.
+          // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 8)
+          OneSignal.promptForPushNotifications(userResponse: { accepted in
+            print("User accepted notifications: \(accepted)")
+          })
+          
+          // Set your customer userId
+          // OneSignal.setExternalUserId("userId")
         return true
     }
 
