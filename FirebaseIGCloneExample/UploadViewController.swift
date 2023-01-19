b805ae9d05a5f26c4639d37f47e177d249f9a282
macsoftware <87543117+macsoftware@users.noreply.github.com>
2023-01-19 15:03:23 +0000

    Fleshing out the upload tab so that it uploads an image to the Firebase Storage and then puts an entry in to the Firestore Database. If sucessful, it will take you back to the feed tab

--- FirebaseIGCloneExample/UploadViewController.swift
+++ FirebaseIGCloneExample/UploadViewController.swift
@@ -6,24 +6,85 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseStorage
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController,  UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentTextField: UITextField!
+    @IBOutlet weak var uploadButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        imageView.isUserInteractionEnabled = true
+        let gestRecog = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
+        imageView.addGestureRecognizer(gestRecog)
+    
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func chooseImage(){
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
     }
-    */
-
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func makeAlert(titleInput: String, messageInput: String){
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func uploadButtonClicked(_ sender: Any) {
+        let storage = Storage.storage()
+        let storageRef = storage.reference()
+        
+        let imagesFolder = storageRef.child("media").child("images")
+        
+        if let data = imageView.image?.jpegData(compressionQuality: 0.3){
+            let uuid = UUID().uuidString
+            let imageRef = imagesFolder.child("\(uuid).jpg")
+            imageRef.putData(data, metadata: nil) { metadata, error in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error!" , messageInput: error?.localizedDescription ?? "Error")
+                }else{
+                    imageRef.downloadURL { url, error in
+                        if error == nil {
+                            let imageURL = url?.absoluteString
+                            
+                            // DB saving
+                            let firestoreDatabase = Firestore.firestore()
+                            var firestoreRef : DocumentReference? = nil
+                            
+                            let firestorePost = ["imageUrl" : imageURL!, "postedBy" : Auth.auth().currentUser!.email, "postComment" : self.commentTextField.text!, "date" : FieldValue.serverTimestamp(), "likes" : 0] as [String : Any]
+                            
+                            firestoreRef = firestoreDatabase.collection("Posts").addDocument(data: firestorePost, completion: { error in
+                                if error != nil{
+                                    self.makeAlert(titleInput: "Error!", messageInput: error?.localizedDescription ?? "Error")
+                                }else{
+                                    self.imageView.image = UIImage(named: "tap-to-select.png")
+                                    self.commentTextField.text = ""
+                                    self.tabBarController?.selectedIndex = 0 //takes you to the "Feed tab"
+                                }
+                            })
+                            
+                        }else{
+                        }
+                    }
+                }
+            }
+        }
+        
+    }
+    
 }
